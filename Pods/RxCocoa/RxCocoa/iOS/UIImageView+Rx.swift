bf17e55f17920aeb4cfbd55fb11cbf1047be7456
Morshed Alam <morshed.alam@gagagugu.com>
2018-03-09 00:59:17 +0600

    pod file updated for swift 4.0

--- Pods/RxCocoa/RxCocoa/iOS/UIImageView+Rx.swift
+++ Pods/RxCocoa/RxCocoa/iOS/UIImageView+Rx.swift
@@ -8,54 +8,17 @@
 
 #if os(iOS) || os(tvOS)
 
-import Foundation
-#if !RX_NO_MODULE
 import RxSwift
-#endif
 import UIKit
 
-extension UIImageView {
+extension Reactive where Base: UIImageView {
     
-    /**
-    Bindable sink for `image` property.
-    */
-    public var rx_image: AnyObserver<UIImage?> {
-        return self.rx_imageAnimated(nil)
-    }
-    
-    /**
-    Bindable sink for `image` property.
-    
-    - parameter transitionType: Optional transition type while setting the image (kCATransitionFade, kCATransitionMoveIn, ...)
-    */
-    public func rx_imageAnimated(transitionType: String?) -> AnyObserver<UIImage?> {
-        return AnyObserver { [weak self] event in
-            MainScheduler.ensureExecutingOnScheduler()
-            
-            switch event {
-            case .Next(let value):
-                if let transitionType = transitionType {
-                    if value != nil {
-                        let transition = CATransition()
-                        transition.duration = 0.25
-                        transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
-                        transition.type = transitionType
-                        self?.layer.addAnimation(transition, forKey: kCATransition)
-                    }
-                }
-                else {
-                    self?.layer.removeAllAnimations()
-                }
-                self?.image = value
-            case .Error(let error):
-                bindingErrorToInterface(error)
-                break
-            case .Completed:
-                break
-            }
+    /// Bindable sink for `image` property.
+    public var image: Binder<UIImage?> {
+        return Binder(base) { imageView, image in
+            imageView.image = image
         }
     }
-    
 }
 
 #endif
